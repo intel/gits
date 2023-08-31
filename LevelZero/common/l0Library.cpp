b86140bb1658e1064ed122100c996a65d0fa3518
Kozlowski, Marek <marek.kozlowski@intel.com>
2023-08-31 00:21:59 -0700

    [L0] Use `std::move` instead of copy

--- LevelZero/common/l0Library.cpp
+++ LevelZero/common/l0Library.cpp
@@ -29,7 +29,8 @@ namespace l0 {
     *
     * CLibrary class constructor.
     */
-CLibrary::CLibrary(gits::CLibrary::state_creator_t stc) : gits::CLibrary(ID_LEVELZERO, stc) {}
+CLibrary::CLibrary(gits::CLibrary::state_creator_t stc)
+    : gits::CLibrary(ID_LEVELZERO, std::move(stc)) {}
 
 CLibrary::~CLibrary() {
   delete CStateDynamic::InstancePtr();
