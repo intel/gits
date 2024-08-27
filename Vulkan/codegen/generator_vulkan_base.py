5a8012fc79ed4f3307011caf6fbc1833f4ec64fa
Grzegorz Grzybowski <grzegorz.grzybowski@intel.com>
2024-08-27 19:29:26 +0000

    [Vk] Add dataclasses to generator_vulkan_base.py
    They are not being used yet. They will be wired up later.

--- Vulkan/codegen/generator_vulkan_base.py
+++ Vulkan/codegen/generator_vulkan_base.py
@@ -9,6 +9,7 @@
 # ===================== end_copyright_notice ==============================
 
 import enum
+from dataclasses import dataclass, field
 from enum import IntFlag
 
 # TODO: use `@verify(NAMED_FLAGS)` when Python 3.11 becomes available.
@@ -31,12 +32,110 @@ class FuncType(IntFlag):
     NEXT_SUBPASS = 4096
 
 
-class FuncLevel(enum.Enum):  # Direct Enum import would clash with Vulkan Enums.
+class FuncLevel(enum.Enum):  # Direct Enum import would clash with Enum function.
     """Vulkan API function level."""
 
     PROTOTYPE = 0
     GLOBAL = 1
     INSTANCE = 2
+    DEVICE = 4
+
+
+@dataclass(frozen=True, kw_only=True)
+class Argument:
+    """Argument of a Token."""
+
+    name: str
+    type: str
+    wrap_type: str | None = None
+    wrap_params: str | None = None
+    remove_mapping: bool = False
+
+
+@dataclass(frozen=True, kw_only=True)
+class ReturnValue(Argument):
+    """Return value of a Token."""
+
+    # Only `type` is being used, hardcode the rest.
+    name: str = field(init=False, default='return_value')
+    type: str
+    wrap_type: str | None = field(init=False, default=None)
+    wrap_params: str | None = field(init=False, default=None)
+    remove_mapping: bool = field(init=False, default=False)
+
+
+@dataclass(frozen=True, kw_only=True)
+class Token:
+    """API call (or an internal GITS action)."""
+
+    name: str
+    enabled: bool
+    function_type: FuncType
+    level: FuncLevel = FuncLevel.DEVICE
+    version: int = 0
+    state_track: bool | str = False
+    recorder_wrap: bool | str = False
+    exec_post_recorder_wrap: bool = False
+    recorder_exec_wrap: bool = False
+    run_wrap: bool | str = False
+    ccode_wrap: bool = False
+    ccode_write_wrap: bool = False
+    ccode_post_action_needed: bool = False
+    plugin_wrap: bool = False
+    custom_driver: bool = False
+    end_frame_tag: bool = False
+    pre_token: str | None = None
+    post_token: str | None = None
+    token_cache: str | None = None  # TODO: Make it an enum.
+    return_value: ReturnValue
+    args: list[Argument]
+
+
+@dataclass(frozen=True, kw_only=True)
+class Field:
+    """Member variable (field) of a VkStruct."""
+
+    name: str
+    type: str
+    wrap_type: str | None = None
+    wrap_params: str | None = None
+    count: str | None = None
+    log_condition: str | None = None
+
+
+@dataclass(frozen=True, kw_only=True)
+class VkStruct:
+    """Vulkan's C/C++ struct or union."""
+
+    name: str
+    enabled: bool
+    type: str = 'struct'  # Other possible values include 'union'.
+    version: int = 0
+    custom: bool = False
+    declare_array: bool = False
+    declare_array_of_arrays: bool = False
+    constructor_arguments: str | None = None
+    constructor_wrap: bool = False
+    declaration_needed_wrap: bool = False
+    pass_struct_storage: bool = False
+    fields: list[Field]
+
+
+@dataclass(frozen=True, kw_only=True)
+class Enumerator:
+    """Member of a VkEnum."""
+
+    name: str
+    value: int
+
+
+@dataclass(frozen=True, kw_only=True)
+class VkEnum:  # TODO: Remove the "Vk" prefix here and in VkStruct when Enum is renamed to enum?
+    """Vulkan's C/C++ enum."""
+
+    name: str
+    size: int = 32
+    enumerators: list[Enumerator]
 
 
 enums_table = []
