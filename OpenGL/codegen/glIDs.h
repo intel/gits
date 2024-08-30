c581e191359afdfd14d8da1b8e1c165c38d2b43a
Grzegorz Grzybowski <grzegorz.grzybowski@intel.com>
2024-08-30 15:49:07 +0000

    [OGL] Add self-description to glIDs.h

--- OpenGL/codegen/glIDs.h
+++ OpenGL/codegen/glIDs.h
@@ -6,6 +6,9 @@
 //
 // ===================== end_copyright_notice ==============================
 
+// This file stores OpenGL API call IDs. When enabling new functions, their IDs
+// must be appended at the end. Reordering will break stream compatibility.
+
 // clang-format off
 ID_GL_ACCUM,
 ID_GL_ACCUMX_OES,
