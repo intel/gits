f6f17ab7536f2d60612de30840d5440a9215f54c
Vinals Gangolells, Guillem <guillem.vinals.gangolells@intel.com>
2025-01-07 12:40:25 +0000

    Update copyright header

--- Scripts/Installer/install_vulkan_layer.bat
+++ Scripts/Installer/install_vulkan_layer.bat
@@ -1,7 +1,7 @@
 @echo off
 REM ===================== begin_copyright_notice ============================
 REM
-REM Copyright (C) 2023-2024 Intel Corporation
+REM Copyright (C) 2023-2025 Intel Corporation
 REM
 REM SPDX-License-Identifier: MIT
 REM
