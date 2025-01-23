f79f1d7017bcbea2a29f0b29aca6f324cb1faab9
Aaron Wantoch Rekowski <aaron.wantoch.rekowski@intel.com>
2025-01-23 13:32:41 +0000

    [CL] Remove script for adding tags to generator_cl.py

--- OpenCL/codegen/tag.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sed -i -E "/tag=/! s/name='(.*)',type/name='\1',tag='in',type/g" generator_cl.py #Add tag where it doesn't exist default is in
-sed -i -E "/type='.*\*'/ s/tag='in'/tag='out'/g" generator_cl.py #Change from tag in to out if type is a pointer
-sed -i -E "/type='const .*'/ s/tag='out'/tag='in'/g" generator_cl.py #Change from tag out to in if pointer is const
