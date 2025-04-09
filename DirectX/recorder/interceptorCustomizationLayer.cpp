9783fcd1335be7c492410567618ddb004fa9301d
Maciej Wieczorek <maciej1.wieczorek@intel.com>
2025-04-09 12:24:10 +0000

    [DX12] Capture - handle swap chain buffers shared reference count

--- DirectX/recorder/interceptorCustomizationLayer.cpp
+++ DirectX/recorder/interceptorCustomizationLayer.cpp
@@ -18,6 +18,47 @@
 namespace gits {
 namespace DirectX {
 
+void InterceptorCustomizationLayer::post(IDXGISwapChainGetBufferCommand& c) {
+  if (c.result_.value == S_OK) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    swapChainByBufferKey_[c.ppSurface_.key] = c.object_.key;
+    buffersBySwapChainKey_[c.object_.key].push_back(static_cast<IUnknown*>(*c.ppSurface_.value));
+  }
+}
+
+void InterceptorCustomizationLayer::pre(IUnknownReleaseCommand& c) {
+  // Handle swap chain buffers shared reference count
+  // Needs to be done in pre layer, because objects need to be alive for removeWrapper call
+
+  c.object_.value->AddRef();
+  auto result = c.object_.value->Release();
+  if (result != 1) {
+    return;
+  }
+
+  std::lock_guard<std::mutex> lock(mutex_);
+  auto itSwapChain = swapChainByBufferKey_.find(c.object_.key);
+  if (itSwapChain == swapChainByBufferKey_.end()) {
+    return;
+  }
+  auto itBuffers = buffersBySwapChainKey_.find(itSwapChain->second);
+  if (itBuffers == buffersBySwapChainKey_.end()) {
+    return;
+  }
+
+  for (IUnknown* bufferObject : itBuffers->second) {
+    // Buffer which is currently beeing released is removed in IUnknownWrapper::Release
+    if (bufferObject == c.object_.value) {
+      continue;
+    }
+    auto* bufferWrapper = CaptureManager::get().findWrapper(bufferObject);
+    swapChainByBufferKey_.erase(bufferWrapper->getKey());
+    CaptureManager::get().removeWrapper(bufferWrapper);
+  }
+  swapChainByBufferKey_.erase(itSwapChain);
+  buffersBySwapChainKey_.erase(itBuffers);
+}
+
 void InterceptorCustomizationLayer::pre(D3D12CreateDeviceCommand& command) {
   Microsoft::WRL::ComPtr<IDXGIAdapter> adapter;
   if (command.pAdapter_.value) {
--- DirectX/recorder/interceptorCustomizationLayer.h
+++ DirectX/recorder/interceptorCustomizationLayer.h
@@ -22,8 +22,16 @@ class InterceptorCustomizationLayer : public Layer {
 public:
   InterceptorCustomizationLayer() : Layer("InterceptorCustomization") {}
 
+  void post(IDXGISwapChainGetBufferCommand& command) override;
+  void pre(IUnknownReleaseCommand& command) override;
+
   void pre(D3D12CreateDeviceCommand& command) override;
   void post(D3D12CreateDeviceCommand& command) override;
+
+private:
+  std::unordered_map<unsigned, unsigned> swapChainByBufferKey_;
+  std::unordered_map<unsigned, std::vector<IUnknown*>> buffersBySwapChainKey_;
+  std::mutex mutex_;
 };
 
 } // namespace DirectX
