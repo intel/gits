6db4594765952b153b8cebc0df0b1b696b3cd07e
Maciej Wieczorek <maciej1.wieczorek@intel.com>
2025-12-11 16:43:34 +0000

    [DX12] Code generation - glob files for clang-format after running the generator

---
+++ DirectX/codegen/run_clang_format.cmake
@@ -0,0 +1,18 @@
+# ===================== begin_copyright_notice ============================
+#
+# Copyright (C) 2023-2025 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+#
+# ===================== end_copyright_notice ==============================
+
+file(GLOB_RECURSE FILES
+  "${OUT_DIR}/*Auto.cpp"
+  "${OUT_DIR}/*Auto.h"
+)
+
+if(FILES)
+  execute_process(
+    COMMAND ${CLANG_FORMAT} -i -style=file ${FILES}
+  )
+endif()
