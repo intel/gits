ae7ad20f1c37f731b5cdf464ca2650c8ba949e21
Gonakhchian, Viacheslav <viacheslav.gonakhchian@intel.com>
2026-06-01 16:46:33 +0000

    [common] Add stb_implementation static library to avoid problems when defining STB implementation in multiple places
    Fix CMakeLists.txt to import stb in general case (used by both dx and vulkan - to be merged in the future)

--- DirectX/common/utils/imageWriter.cpp
+++ DirectX/common/utils/imageWriter.cpp
@@ -13,9 +13,7 @@
 #include <DirectXTex.h>
 #include <wincodec.h>
 
-#define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
-#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
 
 namespace gits {
---
+++ common/stb/src/stbImage.cpp
@@ -0,0 +1,12 @@
+// ===================== begin_copyright_notice ============================
+//
+// Copyright (C) 2023-2026 Intel Corporation
+//
+// SPDX-License-Identifier: MIT
+//
+// ===================== end_copyright_notice ==============================
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
