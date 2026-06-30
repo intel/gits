e6aaacb1b2ad2db79ed74164429cdca144da96cb
Vinals Gangolells, Guillem <guillem.vinals.gangolells@intel.com>
2026-06-30 08:36:47 +0000

    [Vulkan] Add missing headers

---
+++ Vulkan2/common/layer_interface/arguments.cpp
@@ -0,0 +1,9 @@
+// ===================== begin_copyright_notice ============================
+//
+// Copyright (C) 2023-2026 Intel Corporation
+//
+// SPDX-License-Identifier: MIT
+//
+// ===================== end_copyright_notice ==============================
+
+#include "arguments.h"
